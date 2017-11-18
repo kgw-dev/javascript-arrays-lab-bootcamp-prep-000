e77361f5c68cde1f8be840a8204d111df76aa1ab
kent winter <kgw700@gmail.com>
2017-11-18 22:33:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,18 @@ function appendKitten (name) {
   var newkittens = [...kittens, name];
   return(newkittens)
 }
+
+function prependKitten (name) {
+  var newkittens = [name, ...kittens];
+  return(newkittens)
+}
+
+function removeLastKitten (name) {
+  var newkittens = kittens.slice(0,kittens.length-1);
+  return(newkittens)
+}
+
+function removeFirstKitten (name) {
+  var newkittens = kittens.slice(1);
+  return(newkittens)
+}
