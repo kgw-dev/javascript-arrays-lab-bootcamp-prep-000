ff114c5ce8178cf7d353355b54b53272607662d8
kent winter <kgw700@gmail.com>
2017-11-18 22:28:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ const app = "I don't do much."
 function destructivelyAppendKitten (name) {
   kittens.push(name);
 }
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten (name) {
+  kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten (name) {
+  kittens.shift(name);
+}
+
+function appendKitten (name) {
+  var newkittens [...x, name];
+  return(newkittens)
+}
