63b5b3b31255ce821173e495eeeb34902ae1cffd
kent winter <kgw700@gmail.com>
2017-11-18 22:30:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function destructivelyRemoveFirstKitten (name) {
 }
 
 function appendKitten (name) {
-  var newkittens = [...x, name];
+  var newkittens = [...kittens, name];
   return(newkittens)
 }
